36e9cde79049ca4bbde2fc8930c09eefbc888caa
vickysassoon03 <vicky@MacBook-Air-de-Victoria.local>
2022-11-15 16:37:22 -0300

    hola

--- js/detail-generes.js
+++ js/detail-generes.js
@@ -1,50 +1,58 @@
-fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key=400f43d154bc968e0f7c02f3b9187c48&language=en-US') 
-  .then (function(response) {
-  return response.json();
 
-}) 
-``
-  .then(function(data){
-    console.log (data)
-    let lista = document.querySelector(".peliculas_populares")
-    let elementoslista= ""
-
-    for (let i = 0; i < data.results.length; i++) {
-      elementoslista += `<h1 class="titulocomedia">Comedia</h1>
-    
-      <h2 class = "titulo_ppl"> Películas de comedia </h2>
-      <section class="peliculas_populares">
-      
-          <article class="pelicula">
-              <a class = "hipervinculo" href="./detail_movie.html">
-                  <img class="imagenPP" src="img/niñera.jpeg" alt="Top Gun"> 
-                  <p class = 'titulo'> Niñera a prueba de balas</p>
-                  <p class = 'estreno'> 21-10-2014</p> 
-              </a>
-          </article>`
-  
-      
-    }
-      console.log(elementoslista);
-      lista.innerHTML = elementoslista; 
-     
-})
-
-.catch(function(error){
-  console.log ('el error fue: ' + error);
-})
+//DETAIL GENERO DE PELICULAS//
 
 
+//usar location.search para obtener y almacenar la query string
+let qs = location.search;
 
-<h1 class="titulocomedia">Comedia</h1>
-    
-    <h2 class = "titulo_ppl"> Películas de comedia </h2>
-    <section class="peliculas_populares">
-    
-        <article class="pelicula">
-            <a class = "hipervinculo" href="./detail_movie.html">
-                <img class="imagenPP" src="img/niñera.jpeg" alt="Top Gun"> 
-                <p class = 'titulo'> Niñera a prueba de balas</p>
-                <p class = 'estreno'> 21-10-2014</p> 
-            </a>
-        </article>
+//transformar la qs en objeto literal
+
+let ol = new URLSearchParams(qs);
+
+//obtener valor de id de la query string//
+let id = ol.get('id_G_Movie');
+
+//armar un fetch //
+let url = `https://api.themoviedb.org/3/genre/movie/list?api_key=400f43d154bc968e0f7c02f3b9187c48&language=en-US${id}`;
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        console.log(data);
+
+        let info = data.results
+
+        //capturo el DOM//
+        let lista = document.querySelector('.peliculas_populares')
+
+        //variable vacia para luego insertar los articulos//
+        let elementosPeliculas = ''
+
+        for (let i = 0; i < info.length; i++) {
+            if (info[i].id == null) {
+                elementosPeliculas +=
+                    `<article class="peliculas">
+     <h4>${info[i].name}</h4>
+     <a href="./detailmovie.html?movie_id=${info[i].id}"> <img src="./img/noImage.png" alt="Portada">
+     </a></article>`
+            } else {
+                elementosPeliculas +=
+                    `<article class="peliculas">
+             <h4>${info[i].name}</h4>
+             <a href="./detailmovie.html?movie_id=${info[i].id}"> <img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}" alt="Portada">
+             </a></article>`
+            }
+        }
+
+        //modifico el DOM//
+        lista.innerHTML = elementosPeliculas;
+    })
+
+    .catch(function (error) {
+        console.log("Error: " + error)
+    })
+
+
