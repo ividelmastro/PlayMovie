450a124f0d44000d8e1fd0f9f112eba9d58278d5
vickysassoon03 <vicky@MacBook-Air-de-Victoria.local>
2022-11-17 11:20:18 -0300

    hola

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,8 +1,9 @@
 //DETALLE DE PELICULAS:
+//Obtengo la qs
 
-let queryString= location.search // aca obtenemos la query string desde la url
+let queryString= location.search
 
-//Transformamos query en objeto literal
+//Construyo objeto literal
 let qsObject = new URLSearchParams(location.search)
 
 //Obtengo el id de la propiedad del objeto literal
@@ -25,44 +26,15 @@ fetch(url)
         let titulo = document.querySelector('.titulo_pelicula');
         let descripcion = document.querySelector('.pelicula_descripcion');
         let img = document.querySelector('.foto_pelicula');
-        let estreno = document.querySelector('.estreno');
-        let sinopsis = document.querySelector('.pelicula_descripcion');
-        let trailer = document.querySelector('.trailer');
-
-        if (data.poster_path == null) {
-            imagen.src = "./img/noImage.png" //cambiar
-        } else {
-            imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
-        }
 
         
         // Agregar la información de la api y mostrarlo en el html
         titulo.innerText = data.original_title;
         descripcion.innerText = data.overview;
-        estreno.innerText = data.release_date;
-        sinopsis.innerText = data.overview;
-        trailer.innerText = null
-
-        let generos = ""
-        let info = data
-        let capturo = document.querySelector(".generos")
-
-        if (info.genre_ids == null || info.genre.ids == 0) {
-            generos += `<p> No se encontraron generos </p>`
-        }
-
-        for (let i = 0; i < info.genre_ids.length; i++) {
-            generos +=
-                `<p><a href="./detail-genres.html?id_G_Movie=${info.genre_ids[i].id}&name_G_Movie=${info.genre_ids[i].name}&tipo=movies">${info.genre_ids[i].name}.  </a></p>`
-        }
-        capturo.innerHTML += generos;
-
-
+        img.src= "https://image.tmdb.org/t/p/w500/${data.poster_path}"
     
  })
 
     .catch(function(e){
         console.log(e);
  })
-
- 
