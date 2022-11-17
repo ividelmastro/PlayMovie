e7d568cffeb9315ac451e18201192e5c5121cd90
ividelmastro <idelmastrorasic@udesa.edu.ar>
2022-11-17 12:06:17 -0300

    detalle

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -8,6 +8,7 @@ let qsObject = new URLSearchParams(location.search)
 
 //Obtengo el id de la propiedad del objeto literal
 let id = qsObject.get("id")
+console.log(id)
 
 //endpoint con el id de la qs
 let url = `https://api.themoviedb.org/3/movie/${id}?api_key=400f43d154bc968e0f7c02f3b9187c48&language=en-US`
@@ -23,18 +24,47 @@ fetch(url)
     .then(function(data){
         console.log(data);
         //capturar cada elemento del html que queremos completar
-        let titulo = document.querySelector('.titulo_pelicula');
-        let descripcion = document.querySelector('.pelicula_descripcion');
         let img = document.querySelector('.foto_pelicula');
+        let titulo = document.querySelector('.titulo_pelicula');
+        let calificacion = document.querySelector('.rating');
+        let fecha = document.querySelector('.fecha_estreno');
+        let duracion = document.querySelector('.duracion');
+        let sinopsis = document.querySelector('.pelicula_descripcion');
 
         
+        if (data.poster_path == null){
+            img.src = "./img/imagen-no-disponible.jpeg"
+        }
+        else{
+            img.src= `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+        }
+        
         // Agregar la información de la api y mostrarlo en el html
-        titulo.innerText = data.original_title;
-        descripcion.innerText = data.overview;
-        img.src= "https://image.tmdb.org/t/p/w500/${data.poster_path}"
-    
- })
+        titulo.innerHTML += data.original_title;
+        sinopsis.innerHTML += data.overview;
+        rating.innerHTML += data.vote_average;
+        fecha.innerHTML += data.release_date;
 
-    .catch(function(e){
-        console.log(e);
- })
+        let generos = ""
+        let info = data
+        let capturo = document.querySelector(".generos")
+
+        if (info.genres == null || info.genres == 0) {
+            generos += `<p> No se encontraron generos </p>`
+        }
+
+        ///DESPUES DE HACER DETAIL GENEROS VER ESTOOOOOOO
+        for (let i = 0; i < info.genres.length; i++) {
+            generos +=
+                `<p><a href="./detail-genres.html?id=${info.genres[i].id}&name_G_Movie=${info.genres[i].name}&tipo=movies">${info.genres[i].name}.  </a></p>`
+        }
+        capturo.innerHTML += generos;
+
+
+
+})
+.catch(function (error) {
+    console.log(error);
+})
+        
+        
